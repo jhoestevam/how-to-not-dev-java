5826c168a263ec88e834ca968068a0c97b17db95
jhoestevam <jhonatanm222@gmail.com>
2018-04-16 15:38:35 -0300

    Nova entidade usuario

---
+++ src/main/java/entity/Usuario.java
@@ -0,0 +1,36 @@
+package entity;
+
+public class Usuario {
+
+    private String nome;
+    private int pontos;
+    private boolean moderador;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+    public boolean tornaModerador(){
+        return this.moderador = true;
+    }
+
+    public boolean isModerador() {
+        return moderador;
+    }
+
+    public void setModerador(boolean moderador) {
+        this.moderador = moderador;
+    }
+}
